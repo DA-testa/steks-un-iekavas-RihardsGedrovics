16951a794637f0221b55102499a463d893f71f77
RihardsGedrovics <125249238+RihardsGedrovics@users.noreply.github.com>
2023-02-18 13:20:56 +0000

    tests2

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
+# python3
+
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-def matchc(bracket, c):
-    if bracket.char == '[' and c == ']':
-        return True
-    if bracket.char == '{' and c == '}':
-        return True
-    if bracket.char == '(' and c == ')':
-        return True
-    return False
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
 
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i+1))
+            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i + 1))
+
         if next in ")]}":
-            if not opening_brackets_stack or not matchc(opening_brackets_stack[-1], next):
+            # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(
+                opening_brackets_stack[-1].char, next
+            ):
                 return i + 1
             opening_brackets_stack.pop()
-    return "Success" if not opening_brackets_stack else opening_brackets_stack[-1].position
+    if opening_brackets_stack:
+        return opening_brackets_stack[-1].position
+    return "Success"
+
 
 def main():
+    choice = input()
     text = input()
-    result = find_mismatch(text)
-    if isinstance(result, int):
-        print(result)
+    mismatch = find_mismatch(text)
+    choice.upper()
+    # Printing answer, write your code here
+    if choice == "I":
+        print(mismatch)
     else:
-        print(result)
+        print(mismatch)
 
-if __name__ == "__main__":
+
+if _name_ == "_main_":
     main()
