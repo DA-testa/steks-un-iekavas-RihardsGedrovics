2c4b9a1cb1d0c04cf4ed16c52972e9ea6b82ebe2
RihardsGedrovics <125249238+RihardsGedrovics@users.noreply.github.com>
2023-02-18 13:22:27 +0000

    tests3

--- main.py
+++ main.py
@@ -14,14 +14,12 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next, i + 1))
+            opening_brackets_stack.append(Bracket(next, i+1))
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(
-                opening_brackets_stack[-1].char, next
-            ):
-                return i + 1
+            if(not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next)):
+                return i+1
             opening_brackets_stack.pop()
     if opening_brackets_stack:
         return opening_brackets_stack[-1].position
@@ -38,7 +36,5 @@ def main():
         print(mismatch)
     else:
         print(mismatch)
-
-
 if _name_ == "_main_":
     main()
